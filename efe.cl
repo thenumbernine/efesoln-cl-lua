0dafba405a7cad6442b32b68ea4f0d538f362024
Christopher Moore <christopher.e.moore@gmail.com>
2016-12-05 18:22:29 +0200

    my codegen has got out of hand

--- efe.cl
+++ efe.cl
@@ -568,3 +568,227 @@ kernel void calc_norm_EinsteinLLs(
 end ?>);
 }
 
+// and here is the code used for updating weights based on the EFE
+
+kernel void calc_dPhi_dgLLs(
+	global sym4* dPhi_dgLLs,
+	global const TPrim_t* TPrims,
+	global const sym4* gLLs,
+	global const sym4* gUUs,
+	global const tensor_4sym4* GammaULLs,
+	global const sym4* EFEs
+) {
+	INIT_KERNEL();
+	
+	global const sym4* gLL = gLLs + index;
+	global const sym4* gUU = gUUs + index;
+	global const tensor_4sym4* GammaULL = GammaULLs + index;
+
+	//this is also in the Ricci computation, but should I be storing it?  is it too big?
+	tensor_44sym4 dGammaLULL;
+	dGammaLULL.s0 = tensor_4sym4_zero;
+	<? for i=0,gridDim-1 do ?>{
+		int4 iL = i;
+		iL.s<?=i?> = max(i.s<?=i?> - 1, 0);
+		int indexL = indexForInt4(iL);
+		global const tensor_4sym4* GammaULL_prev = GammaULLs + indexL;
+		
+		int4 iR = i;
+		iR.s<?=i?> = min(i.s<?=i?> + 1, size.s<?=i?> - 1);
+		int indexR = indexForInt4(iR);
+		global const tensor_4sym4* GammaULL_next = GammaULLs + indexR;
+		
+		dGammaLULL.s<?=i+1?> = tensor_4sym4_scale(
+			tensor_4sym4_sub(*GammaULL_next, *GammaULL_prev),
+			.5 * inv_dx.s<?=i?> );
+	}<? end ?>
+
+	tensor_44sym4 RiemannULLL = (tensor_44sym4){
+<? for a=0,dim-1 do ?>
+		.s<?=a?> = (tensor_4sym4){
+	<? for b=0,dim-1 do ?>
+			.s<?=b?> = (sym4){
+		<? for c=0,dim-1 do ?>
+			<? for d=c,dim-1 do ?>
+				.s<?=c?><?=d?> = 
+					dGammaLULL.s<?=c?>.s<?=a?>.s<?=sym(b,d)?>
+					- dGammaLULL.s<?=d?>.s<?=a?>.s<?=sym(b,c)?> 
+				<? for e=0,dim-1 do ?>
+					+ GammaULL->s<?=a?>.s<?=sym(e,c)?> * GammaULL->s<?=e?>.s<?=sym(b,d)?>
+					- GammaULL->s<?=a?>.s<?=sym(e,d)?> * GammaULL->s<?=e?>.s<?=sym(b,c)?>
+				<? end ?>,
+			<? end ?>
+		<? end ?>
+			},
+	<? end ?>
+		},
+<? end ?>
+	};
+
+	sym4 RicciLL = (sym4){
+<? for a=0,dim-1 do ?>
+	<? for b=a,dim-1 do ?>
+		.s<?=a..b?> = 0.
+		<? for c=0,dim-1 do ?>
+			+ RiemannULLL.s<?=c?>.s<?=a?>.s<?=sym(c,b)?>
+		<? end ?>,
+	<? end ?>
+<? end ?>};
+
+	real RicciUL[4][4] = {
+<? for a=0,dim-1 do ?>
+		{
+	<? for b=0,dim-1 do ?>
+			0.
+		<? for c=0,dim-1 do ?>
+				+ gUU->s<?=sym(a,c)?> * RicciLL.s<?=sym(c,b)?>
+		<? end ?>,
+	<? end ?>
+		},
+<? end ?>
+	};
+
+	sym4 RicciUU = (sym4){
+<? for a=0,dim-1 do
+	for b=a,dim-1 do ?>
+		.s<?=a..b?> = 0.
+	<?	for c=0,dim-1 do ?>
+		 + RicciUL[<?=a?>][<?=c?>] * gUU->s<?=sym(c,b)?>
+	<?	end ?>,
+<?	end
+end ?>
+	};
+
+	real Gaussian = 0.
+<? for a=0,dim-1 do ?>
+		+ RicciUL[<?=a?>][<?=a?>]
+<? end ?>;
+
+
+
+	real GammaUUL[4][4][4] = {
+<? for a=0,dim-1 do ?>
+	{<? for b=0,dim-1 do ?>
+		{<? for c=0,dim-1 do ?>
+			0.
+			<? for d=0,dim-1 do ?>
+			+ GammaULL->s<?=a?>.s<?=sym(d,c)?> * gUU->s<?=sym(d,b)?>
+			<? end ?>,
+		<? end ?>},
+	<? end ?>},
+<? end ?>
+	};
+
+	//dR_ab/dg_pq
+	tensor_sym4sym4 dRLL_dgLL = (tensor_sym4sym4){
+<? for e=0,dim-1 do ?>
+	<? for f=e,dim-1 do ?>
+		.s<?=e?><?=f?> = (sym4){
+		<? for a=0,dim-1 do ?>
+			<? for b=a,dim-1 do ?>
+				.s<?=a?><?=b?> = 0.
+				<? for c=0,dim-1 do ?>
+					+ GammaUUL[<?=e?>][<?=c?>][<?=c?>] * GammaULL->s<?=f?>.s<?=sym(b,a)?>
+					- GammaUUL[<?=e?>][<?=c?>][<?=b?>] * GammaULL->s<?=f?>.s<?=sym(c,a)?>
+					- gUU->s<?=sym(c,e)?> * RiemannULLL.s<?=f?>.s<?=a?>.s<?=sym(c,b)?>
+				<? end ?>,
+			<? end ?>
+		<? end ?>
+		},
+	<? end ?>
+<? end ?>
+	};
+
+	//dG_ab/dg_pq = tensor_sym4sym4.s[pq].s[ab]
+	tensor_sym4sym4 dEinsteinLL_dgLL = (tensor_sym4sym4){
+<? for e=0,dim-1 do ?>
+	<? for f=e,dim-1 do ?>
+		.s<?=e?><?=f?> = (sym4){
+		<? for a=0,dim-1 do ?>
+			<? for b=a,dim-1 do ?>
+			.s<?=a?><?=b?> = dRLL_dgLL.s<?=e?><?=f?>.s<?=a?><?=b?>
+				- .5 * (0.
+				<? if e==a and f==b then ?> + Gaussian <? end ?>
+				+ gLL->s<?=a?><?=b?> * (
+					-RicciUU.s<?=e?><?=f?>
+				<? for c=0,dim-1 do ?>
+					<? for d=0,dim-1 do ?>
+					+ gUU->s<?=sym(c,d)?> * dRLL_dgLL.s<?=e?><?=f?>.s<?=sym(c,d)?>
+					<? end ?>
+				<? end ?>
+				)),
+			<? end ?>
+		<? end ?>},
+	<? end ?>
+<? end ?>
+	};
+
+	global const TPrim_t* TPrim = TPrims + index;
+
+	real4 EU = (real4)(0 <?for i=0,2 do ?>, TPrim->E.s<?=i?> <? end ?>); 
+	real4 EL = sym4_real4_mul(*gLL, EU);
+	real ESq = dot(EL, EU);
+	
+	real4 BU = (real4)(0 <?for i=0,2 do ?>, TPrim->B.s<?=i?> <? end ?>); 
+	real4 BL = sym4_real4_mul(*gLL, BU);
+	real BSq = dot(BL, BU);
+
+	real sqrt_det_g = sqrt(fabs(sym4_det(*gLL)));
+	real3 SL = real3_scale(real3_cross(TPrim->E, TPrim->B), sqrt_det_g);
+
+	tensor_sym4sym4 d_8piTLL_dgLL = (tensor_sym4sym4){
+<? for e=0,dim-1 do ?>
+	<? for f=e,dim-1 do ?>
+		.s<?=e?><?=f?> = (sym4){
+			//dTEM_00/dg_<?=e?><?=f?>
+			.s00 = <?=(e==0 or f==0) and '0' or 'TPrim.E.s'..(e-1) * TPrim.E.s'..(f-1)' ?>
+					+ <?=(e==0 or f==0) and '0' or 'TPrim.B.s'..(e-1) * TPrim.B.s'..(f-1)' ?>,
+		<? for i=0,subDim-1 do ?>
+			.s0<?=i+1?> = -SL.s<?=i?> * gUU->s<?=e?><?=f?>,
+			<? for j=i,subDim-1 do ?>
+			.s<?=i+1?><?=j+1?> = 0.
+				<? if e==i+1 and f==j+1 then ?> + ESq + BSq <? end ?>
+				+ gLL->s<?=i?><?=j?> * (
+					<?=(e==0 or f==0) and '0' or 'TPrim.E.s'..(e-1) * TPrim.E.s'..(f-1)' ?>
+					+ <?=(e==0 or f==0) and '0' or 'TPrim.B.s'..(e-1) * TPrim.B.s'..(f-1)' ?>
+				)
+				- 2. * (0.
+					<? if e==i+1 then ?>
+					+ EU.s<?=f?> * EL.s<?=j+1?> + BU.s<?=f?> * BL.s<?=j+1?>
+					<? end ?>
+					<? if e==j+1 then ?>
+					+ EU.s<?=f?> * EL.s<?=i+1?> + BU.s<?=f?> * BL.s<?=i+1?>
+					<? end ?>
+				),
+			<? end ?>
+		<? end ?>},
+	<? end ?>
+<? end ?>
+	};
+
+	global const sym4* EFE = EFEs + index;	// G_ab - 8 pi T_ab
+	<? for p=0,dim-1 do ?>
+		<? for q=p,dim-1 do ?>
+	dPhi_dgLLs[index].s<?=p?><?=q?> = 0.
+			<? for a=0,dim-1 do ?>
+				<? for b=0,dim-1 do ?>
+					+ EFE->s<?=sym(a,b)?> * (dEinsteinLL_dgLL.s<?=p?><?=q?>.s<?=sym(a,b)?> - d_8piTLL_dgLL.s<?=p?><?=q?>.s<?=sym(a,b)?>)
+				<? end ?>
+			<? end ?>;
+		<? end ?>
+	<? end ?>
+}
+
+kernel void update_dgLLs(
+	global sym4* gLLs,
+	global const sym4* dPhi_dgLLs
+) {
+	INIT_KERNEL();
+	global sym4* gLL = gLLs + index;
+	global const sym4* dPhi_dgLL = dPhi_dgLLs + index;
+<? for a=0,dim-1 do ?>
+	<? for b=a,dim-1 do ?>
+	gLL.s<?=a..b?> -= dPhi_dgLL.s<?=a..b?> * <?=updateAlpha?>;
+	<? end ?>
+<? end ?>
+}
--- efe.lua
+++ efe.lua
@@ -4,6 +4,12 @@ require 'ext'
 local template = require 'template'
 local vec3d = require 'ffi.vec.vec3d'
 
+-- parameters:
+
+local updateAlpha = 1
+local maxiter = 0
+local bodyName = 'stellarSchwarzschild'	-- TODO use me plz
+
 -- also in hydro-cl
 
 local vec3sz = require 'ffi.vec.create_ffi'(3,'size_t','sz')
@@ -227,16 +233,7 @@ typedef union {
 
 ]], {real=real}))
 
-ffi.cdef(table{
-	typeCode,
-}:concat'\n')
-
--- header includes macros as well as ffi-cdef code
-
-local headerCode = table{
-	typeCode,
-	file['efe.cl'],
-}:concat'\n'
+ffi.cdef(typeCode)
 
 -- allocate buffers
 
@@ -280,24 +277,8 @@ local function compileTemplates(code)
 		initCond = initCond,
 		c = c,
 		G = G,
+		updateAlpha = updateAlpha,
 	})
-end
-
-function compileMetaKernels(mks)
-	local code = compileTemplates(
-		table{headerCode}
-			:append(table.map(mks, function(mk) return mk.code end))
-			:concat'\n'
-	)
-
-	local program = require 'cl.program'{context=ctx, devices={device}, code=code}
-
-	for _,mk in ipairs(mks) do
-		mk.program = program
-		mk.kernel = program:kernel(mk.name, mk.argBuffers:unpack())
-	end
-
-	return program
 end
 
 local function clcall(kernel, ...)
@@ -307,50 +288,15 @@ local function clcall(kernel, ...)
 	cmds:enqueueNDRangeKernel{kernel=kernel, dim=gridDim, globalSize=size:ptr(), localSize=localSize:ptr()}
 end
 
-local MetaKernel = class()
-
-function MetaKernel:init(args)
-	self.name = args.name
-	self.argsOut = args.argsOut
-	self.argsIn = args.argsIn
-	self.argBuffers = table()
-		:append(self.argsOut)
-		:append(self.argsIn)
-		:map(function(arg) return arg.buf end)
-	self.code = template([[
-kernel void <?=self.name?>(
-<?
-local sep = ''
-for _,arg in ipairs(self.argsOut or {}) do ?>
-	<?=sep?>global <?=arg.type?>* <?=arg.name?>
-<?
-	sep = ', '
-end
-for _,arg in ipairs(self.argsIn or {}) do ?>
-	<?=sep?>global const <?=arg.type?>* <?=arg.name?>
-<?
-	sep = ', '
-end
-?>
-) {
-	INIT_KERNEL();
-	<?=args.code?>
-}
-]], {self=self, args=args})
-end
-
-function MetaKernel:__call(...)
-	-- if we get a call request when we have no kernel/program, make sure to get one 
-	if not self.kernel then
-		compileMetaKernels{self}
-	end
-
-	clcall(self.kernel, ...)
-end
-
 -- create code
 
-local program = compileMetaKernels({})
+local code = compileTemplates(table{
+	typeCode,
+	file['efe.cl'],
+}:concat'\n')
+
+local program = require 'cl.program'{context=ctx, devices={device}, code=code}
+
 -- init
 local init_gPrims = program:kernel('init_gPrims', gPrims.buf)
 local init_TPrims = program:kernel('init_TPrims', TPrims.buf)
@@ -383,188 +329,14 @@ I'll try for 2 and hope I have enough memory
 
 local dPhi_dgLLs = MetaBuffer{name='dPhi_dgLL', type='sym4'}
 
-local alpha = 1
-local update_dgLLs = MetaKernel{
-	name = 'update_dgLLs',
-	argsOut = {gLLs},
-	argsIn = {dPhi_dgLLs},
-	code = template([[
-	gLLs[index] = sym4_sub(gLLs[index], sym4_scale(dPhi_dgLLs[index], -<?=alpha?>));
-]], {alpha=alpha}),
-}
+local calc_dPhi_dgLLs = program:kernel('calc_dPhi_dgLLs', dPhi_dgLLs.buf, TPrims.buf, gLLs.buf, gUUs.buf, GammaULLs.buf, EFEs.buf)
 
-local calc_dPhi_dgLL = MetaKernel{
-	name = 'calc_dPhi_dgLL',
-	argsIn = {EFEs, TPrims, gLLs, gUUs, GammaULLs},
-	argsOut = {dPhi_dgLLs},
-	code = [[
-	
-	global const sym4* gLL = gLLs + index;
-	global const sym4* gUU = gUUs + index;
-	global const tensor_4sym4* GammaULL = GammaULLs + index;
-
-	//this is also in the Ricci computation, but should I be storing it?  is it too big?
-	tensor_44sym4 dGammaLULL;
-	dGammaLULL.s0 = tensor_4sym4_zero;
-	<? for i=0,gridDim-1 do ?>{
-		int4 iL = i;
-		iL.s<?=i?> = max(i.s<?=i?> - 1, 0);
-		int indexL = indexForInt4(iL);
-		global const tensor_4sym4* GammaULL_prev = GammaULLs + indexL;
-		
-		int4 iR = i;
-		iR.s<?=i?> = min(i.s<?=i?> + 1, size.s<?=i?> - 1);
-		int indexR = indexForInt4(iR);
-		global const tensor_4sym4* GammaULL_next = GammaULLs + indexR;
-		
-		dGammaLULL.s<?=i+1?> = tensor_4sym4_scale(
-			tensor_4sym4_sub(*GammaULL_next, *GammaULL_prev),
-			.5 * inv_dx.s<?=i?> );
-	}<? end ?>
-
-	tensor_44sym4 RiemannULLL = (tensor_44sym4){
-<? for a=0,dim-1 do ?>
-		.s<?=a?> = (tensor_4sym4){
-	<? for b=0,dim-1 do ?>
-			.s<?=b?> = (sym4){
-		<? for c=0,dim-1 do ?>
-			<? for d=c,dim-1 do ?>
-				.s<?=c?><?=d?> = 
-					dGammaLULL.s<?=c?>.s<?=a?>.s<?=sym(b,d)?>
-					- dGammaLULL.s<?=d?>.s<?=a?>.s<?=sym(b,c)?> 
-				<? for e=0,dim-1 do ?>
-					+ GammaULL->s<?=a?>.s<?=sym(e,c)?> * GammaULL->s<?=e?>.s<?=sym(b,d)?>
-					- GammaULL->s<?=a?>.s<?=sym(e,d)?> * GammaULL->s<?=e?>.s<?=sym(b,c)?>
-				<? end ?>,
-			<? end ?>
-		<? end ?>
-			},
-	<? end ?>
-		},
-<? end ?>
-	};
-
-	real GammaUUL[4][4][4] = {
-<? for a=0,dim-1 do ?>
-	{<? for b=0,dim-1 do ?>
-		{<? for c=0,dim-1 do ?>
-			0.
-			<? for d=0,dim-1 do ?>
-			+ GammaULL->s<?=a?>.s<?=sym(d,c)?> * gUU->s-><?=sym(d,b)?>
-			<? end ?>,
-		<? end ?>},
-	<? end ?>},
-<? end ?>
-	};
-
-	tensor_44sym4 RiemannULLL = (tensor_44sym4){
-	};
-
-	//dR_ab/dg_pq
-	tensor_sym4sym4 dRLL_dgLL = (tensor_sym4sym4){
-<? for e=0,dim-1 do ?>
-	<? for f=e,dim-1 do ?>
-		.s<?=e?><?=f?> = (sym4){
-		<? for a=0,dim-1 do ?>
-			<? for b=a,dim-1 do ?>
-				.s<?=a?><?=b?> = 0.
-				<? for c=0,dim-1 do ?>
-					+ GammaUUL[<?=e?>][<?=c?>][<?=c?>] * GammaULL.s<?=f?>.s<?=sym(b,a)?>
-					- GammaUUL[<?=e?>][<?=c?>][<?=b?>] * GammaULL.s<?=f?>.s<?=sym(c,a)?>
-					- gUU->s<?=sym(c,e)?> * RiemannULLL.s<?=f?>.s<?=a?>.s<?=sym(c,b)?>
-				<? end ?>,
-			<? end ?>
-		<? end ?>
-		},
-	<? end ?>
-<? end ?>
-	};
-
-	//dG_ab/dg_pq = tensor_sym4sym4.s[pq].s[ab]
-	tensor_sym4sym4 dEinsteinLL_dgLL = (tensor_sym4sym4){
-<? for e=0,dim-1 do ?>
-	<? for f=e,dim-1 do ?>
-		.s<?=e?><?=f?> = (sym4){
-		<? for a=0,dim-1 do ?>
-			<? for b=a,dim-1 do ?>
-			.s<?=a?><?=b?> = dRLL_dgLL.s<?=e?><?=f?>.s<?=a?><?=b?>
-				- .5 * (0.
-				<? if e==a and f==b then ?> + Gaussian <? end ?>
-				+ gLL->s<?=a?><?=b?> * (
-					-RUU.s<?=e?><?=f?>
-				<? for c=0,dim-1 do ?>
-					<? for d=0,dim-1 do ?>
-						gUU->s<?=sym(c,d)?> * dRLL_dgLL.s<?=e?><?=f?>.s<?=sym(c,d)?>
-					<? end ?>
-				<? end ?>
-				)),
-			<? end ?>
-		<? end ?>},
-	<? end ?>
-<? end ?>
-	};
-
-	global const TPrim_t* TPrim = TPrims + index;
-
-	real4 EU = (real4)(0 <?for i=0,2 do ?>, TPrim->E.s<?=i?> <? end ?>); 
-	real4 EL = sym4_real4_mul(*gLL, EU);
-	real ESq = dot(EL, EU);
-	
-	real4 BU = (real4)(0 <?for i=0,2 do ?>, TPrim->B.s<?=i?> <? end ?>); 
-	real4 BL = sym4_real4_mul(*gLL, BU);
-	real BSq = dot(BL, BU);
-
-	real sqrt_det_g = sqrt(fabs(sym4_det(*gLL)));
-	real3 SL = real3_scale(real3_cross(TPrim->E, TPrim->B), sqrt_det_g);
-
-	tensor_sym4sym4 d_8piTLL_dgLL = (tensor_sym4sym4){
-<? for e=0,dim-1 do ?>
-	<? for f=e,dim-1 do ?>
-		.s<?=e?><?=f?> = (sym4){
-			//dTEM_00/dg_<?=p?><?=q?>
-			.s00 = <?=(e==0 or f==0) and '0' or 'TPrim.E.s'..(e-1) * TPrim.E.s'..(f-1)' ?>
-					+ <?=(e==0 or f==0) and '0' or 'TPrim.B.s'..(e-1) * TPrim.B.s'..(f-1)' ?>,
-		<? for i=0,subDim-1 do ?>
-			.s0<?=i+1?> = -SL.s<?=i?> * gUU->s<?=e?><?=f?>,
-			<? for j=i,subDim-1 do ?>
-			.s<?=i+1?><?=j+1?> = 0.
-				<? if e==i+1 and f==j+1 then ?> + ESq + BSq <? end ?>
-				+ gLL->s<?=i?><?=j?> * (
-					<?=(e==0 or f==0) and '0' or 'TPrim.E.s'..(e-1) * TPrim.E.s'..(f-1)' ?>
-					+ <?=(e==0 or f==0) and '0' or 'TPrim.B.s'..(e-1) * TPrim.B.s'..(f-1)' ?>
-				)
-				- 2. * (0.
-					<? if e==i+1 then ?>
-					+ EU.s<?=f?> * EL.s<?=j+1?> + BU.s<?=f?> * BL.s<?=j+1?>
-					<? end ?>
-					<? if e==j+1 then ?>
-					+ EU.s<?=f?> * EL.s<?=i+1?> + BU.s<?=f?> * BL.s<?=i+1?>
-					<? end ?>
-				),
-			<? end ?>
-		<? end ?>},
-	<? end ?>
-<? end ?>
-	};
-
-	global const sym4* EFE = EFEs + index;	// G_ab - 8 pi T_ab
-	<? for p=0,dim-1 do ?>
-		<? for q=p,dim-1 do ?>
-	dPhi_dgLLs[index].s<?=p?><?=q?> = 0.
-			<? for a=0,dim-1 do ?>
-				<? for b=0,dim-1 do ?>
-					+ EFE->s<?=sym(a,b)?> * (dEinsteinLL_dgLL.s<?=p?><?=q?>.s<?=sym(a,b)?> - d_8piTLL_dgLL.s<?=p?><?=q?>.s<?=sym(a,b)?>)
-				<? end ?>
-			<? end ?>;
-		<? end ?>
-	<? end ?>
-]],
-}
+local update_dgLLs = program:kernel('update_dgLLs', gLLs.buf, dPhi_dgLLs.buf)
 
 
-local maxiter = 0
 for i=1,maxiter do
-	calc_dPhi_dgLL()
+	clcall(calc_dPhi_dgLLs)
+	clcall(update_dgLLs)
 end
 
 --[[ 
