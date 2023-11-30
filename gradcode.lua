57e66a583554bc9b1d8be4411ca15d6fb8641cb0
Christopher Moore <christopher.e.moore@gmail.com>
2023-11-30 18:17:32 +0700

    more

--- gradcode.lua
+++ gradcode.lua
@@ -2,16 +2,23 @@
 local table = require 'ext.table'
 local range = require 'ext.range'
 local tolua = require 'ext.tolua'
-require 'symmath'.setup{
-	implicitVars=true}
+require 'symmath'.setup()
 require 'symmath.export.MathJax'.setup{
 	title = 'generating my gradient descent code',
 }
 
-g = var'g'
-G = var'G'
-R = var'R'
-Gamma = var'\\Gamma'
+local t,x,y,z = vars('t', 'x', 'y', 'z')
+local C = Tensor.Chart{coords={t,x,y,z}}
+local CS = Tensor.Chart{coords={x,y,z}, symbols='ijklmn'}
+local Ct = Tensor.Chart{coords={t}, symbols='t'}
+local Cx = Tensor.Chart{coords={x}, symbols='x'}
+local Cy = Tensor.Chart{coords={y}, symbols='y'}
+local Cz = Tensor.Chart{coords={z}, symbols='z'}
+
+local g = var'g'
+local G = var'G'
+local R = var'R'
+local Gamma = var'\\Gamma'
 
 local n = 1	-- nbhd size
 
@@ -24,22 +31,69 @@ local function index(t, ...)
 	return index(t[i], select(2, ...))
 end
 
+--[[ don't think I'm using this at the moment ...
 local xs = Array:lambda(offsets, function(i,j,k)
-	return var('x_{'..table{i-n-1,j-n-1,k-n-1}:concat', '..'}')
+	--[=[
+	return Tensor('^i', function(a)
+		return var('x^'..a..'['..table{i-n-1,j-n-1,k-n-1}:concat','..']')
+	end)
+	--]=]
+	-- [=[ notice :makeDense uses the correct # of dims for index 'i' (being 3) but uses the letters from txyz instead of xyz ...
+	return var('x['..table{i-n-1,j-n-1,k-n-1}:concat','..']')'^i':makeDense()
+	--]=]
 end)
---printbr(xs)
+printbr(xs)
+do return end
+--]]
+
+local function removeSymmetricDuplicates(T)
+	-- remove symmetric duplicate variables
+	for a=1,3 do
+		for b=a+1,4 do
+			T[b][a] = T[a][b]
+		end
+	end
+end
 
 local Phi = var'\\Phi'
 local all_gLLs = table()
 local gs = Array:lambda(offsets, function(i,j,k)
-	local g_ijk = var('g_{'..table{i-n-1,j-n-1,k-n-1}:concat', '..'}')
-	all_gLLs:insert(g_ijk'_pq')
+	local g_ijk = var('g['..table{i-n-1,j-n-1,k-n-1}:concat', '..']')'_ab':makeDense()
+	removeSymmetricDuplicates(g_ijk)
+	for ab,g_ijk_ab in g_ijk:iter() do
+		g_ijk_ab.isMetric = true	-- hack for differentiation
+		all_gLLs:insert(g_ijk_ab)
+	end
 	return g_ijk
 end)
-Phi:setDependentVars(gs[n+1][n+1][n+1]'_ab')
 
+-- I'm going to make a separate copy of gs in raised-form
+-- for the sake of using dense-tensors in raised and lowered
+-- however this might get ugly when differentiating wrt the lower specific-index metric ...
+local all_gUU = table()
+local gUs = Array:lambda(offsets, function(i,j,k)
+	local gU_ijk = var('g['..table{i-n-1,j-n-1,k-n-1}:concat', '..']')'^ab':makeDense()
+	removeSymmetricDuplicates(gU_ijk)
+	for ab,g_ijk_ab in gU_ijk:iter() do
+		g_ijk_ab.isMetric = true	-- hack for differentiation
+		all_gUU:insert(g_ijk_ab)
+	end
+	return gU_ijk
+end)
+
+do
+	local deps = table()
+	for a=1,4 do
+		for b=1,4 do
+			deps:insert(gs[n+1][n+1][n+1][a][b])
+		end
+	end
+	-- ... is phi only dependent on the center g? nah ...
+	--Phi:setDependentVars(deps:unpack())
+	Phi:setDependentVars(all_gLLs:unpack())
+end
 local Phis = Array:lambda(offsets, function(i,j,k)
-	local Phi_ijk = var('\\Phi_{'..table{i-n-1,j-n-1,k-n-1}:concat', '..'}')
+	local Phi_ijk = var('\\Phi['..table{i-n-1,j-n-1,k-n-1}:concat', '..']')
 	Phi_ijk:setDependentVars(all_gLLs:unpack())
 	return Phi_ijk
 end)
@@ -58,57 +112,91 @@ sum = sum()
 local Phi_def = Phi:eq(sum)
 printbr(Phi_def)
 
-local grad_Phi_def = Phi_def:diff(gs[n+1][n+1][n+1]'_pq')()
+-- hmm .... have to do this per a,b of g_ab ...
+local p = 1
+local q = 1
+printbr("considering the gradient for", gs[n+1][n+1][n+1][p][q])
+local grad_Phi_def = Phi_def:diff(gs[n+1][n+1][n+1][p][q])()
 printbr(grad_Phi_def)
 
 local EFEs = Array:lambda(offsets, function(i,j,k)
-	local EFE_ijk = var('EFE_{'..table{i-n-1, j-n-1, k-n-1}:concat', '..'}')
-	EFE_ijk'_ab':setDependentVars(all_gLLs:unpack())
+	local EFE_ijk = var('EFE['..table{i-n-1, j-n-1, k-n-1}:concat', '..']')'_ab':makeDense()
+	removeSymmetricDuplicates(EFE_ijk)
+	for ab,EFE_ijk_ab in EFE_ijk:iter() do
+		EFE_ijk_ab:setDependentVars(all_gLLs:unpack())
+	end
 	return EFE_ijk
 end)
-printbr(Phis[n+1][n+1][n+1]:eq(frac(1,2) * EFEs[n+1][n+1][n+1]'_ab'^2))
+-- this is our energy function to minimize, so I don't think it matters if you double up the symmetries by summing all a,b, or if you just use the upper/lower triangular
+local function make_Phi_def(i,j,k)
+	local sum = 0
+	for a=1,4 do
+		for b=a,4 do
+			sum = sum + EFEs[i][j][k][a][b]^2
+		end
+	end
+	return Phis[i][j][k]:eq(frac(1,2) * sum)
+end
+printbr(make_Phi_def(n+1, n+1, n+1))
 --printbr(EFEs)
 
-for ijk in EFEs:iter() do
-	local EFE_ijk = EFEs[ijk]
-	local Phi_ijk = Phis[ijk]
-	grad_Phi_def = grad_Phi_def:subst(Phi_ijk:eq(frac(1,2) * EFE_ijk'_ab'^2))
+for i=1,2*n+1 do
+	for j=1,2*n+1 do
+		for k=1,2*n+1 do
+			grad_Phi_def = grad_Phi_def:subst(make_Phi_def(i,j,k))
+		end
+	end
 end
 
 printbr(grad_Phi_def)
 grad_Phi_def = grad_Phi_def()
 printbr(grad_Phi_def)
 
-local function setDependents_D2gLL(x_ijk, ijk)
+local function setDependents_D2gLL(T, i, j, k)
 	-- [[ 2nd derivative = Linf-norm
 	local stencil_gLLs = table()
 	for di=-n,n do
 		for dj=-n,n do
 			for dk=-n,n do
-				local g_ijk = index(gs, ijk[1]+di, ijk[2]+dj, ijk[3]+dk)
+				local g_ijk = index(gs, i+di, j+dj, k+dk)
 				if g_ijk then
-					stencil_gLLs:insert(g_ijk'_pq')
+					for a=1,4 do
+						for b=1,4 do
+							stencil_gLLs:insert(g_ijk[a][b])
+						end
+					end
 				end
 			end
 		end
 	end
-	x_ijk:setDependentVars(stencil_gLLs:unpack())
+	T:setDependentVars(stencil_gLLs:unpack())
 	--]]
 end
 
 local Gs = Array:lambda(offsets, function(i,j,k)
-	local G_ijk = var('G_{'..table{i-n-1,j-n-1,k-n-1}:concat', '..'}')
-	setDependents_D2gLL(G_ijk'_ab', {i, j, k})
+	local G_ijk = var('G['..table{i-n-1,j-n-1,k-n-1}:concat', '..']')'_ab':makeDense()
+	removeSymmetricDuplicates(G_ijk)
+	for ab,G_ijk_ab in G_ijk:iter() do
+		setDependents_D2gLL(G_ijk_ab, i, j, k)
+	end
 	return G_ijk
 end)
 local Ts = Array:lambda(offsets, function(i,j,k)
-	local T_ijk = var('T_{'..table{i-n-1,j-n-1,k-n-1}:concat', '..'}')
-	local g_ijk = gs[i][j][k]
-	T_ijk'_ab':setDependentVars(g_ijk'_ab')
+	local T_ijk = var('T['..table{i-n-1,j-n-1,k-n-1}:concat', '..']')'_ab':makeDense()
+	removeSymmetricDuplicates(T_ijk)
+	local deps = table()
+	for u=1,4 do
+		for v=1,4 do
+			deps:insert(gs[i][j][k][u][v])
+		end
+	end
+	for ab,T_ijk_ab in T_ijk:iter() do
+		T_ijk_ab:setDependentVars(deps:unpack())
+	end
 	return T_ijk
 end)
-for ijk in Gs:iter() do
-	local expr = (EFEs[ijk]'_ab':eq(Gs[ijk]'_ab' - 8 * pi * Ts[ijk]'_ab')):diff(gs[n+1][n+1][n+1]'_pq')()
+for ijkab in Gs:iter() do
+	local expr = (EFEs[ijkab]:eq(Gs[ijkab] - 8 * pi * Ts[ijkab])):diff(gs[n+1][n+1][n+1][p][q])()
 	grad_Phi_def = grad_Phi_def:subst(expr)
 end
 printbr(grad_Phi_def)
@@ -118,45 +206,83 @@ printbr(grad_Phi_def)
 --printbr(grad_Phi_def)
 
 local Rs = Array:lambda(offsets, function(i,j,k)
-	local R_ijk = var('R_{'..table{i-n-1,j-n-1,k-n-1}:concat', '..'}')
-	setDependents_D2gLL(R_ijk'_ab', {i, j, k})
-	--R_ijk'_ab':setDependentVars(gs[i][j][k]'_ab')
-	--R_ijk'_ab':setDependentVars(all_gLLs:unpack())
+	local R_ijk = var('R['..table{i-n-1,j-n-1,k-n-1}:concat', '..']')'_ab':makeDense()
+	removeSymmetricDuplicates(R_ijk)
+	for ab,R_ijk_ab in R_ijk:iter() do
+		setDependents_D2gLL(R_ijk_ab, i, j, k)
+		--R_ijk[ab]:setDependentVars(gs[i][j][k][ab])
+		--R_ijk[ab]:setDependentVars(all_gLLs:unpack())
+	end
 	return R_ijk
 end)
-for ijk in Rs:iter() do
-	grad_Phi_def = grad_Phi_def:subst(
-		Gs[ijk]'_ab':eq(Rs[ijk]'_ab'  - frac(1,2) * gs[ijk]'_ab' * gs[ijk]'^uv' * Rs[ijk]'_uv')
-	)
+for i=1,2*n+1 do
+	for j=1,2*n+1 do
+		for k=1,2*n+1 do
+			local ijk = {i,j,k}
+			-- if you use this again, then consider caching it 
+			local Gaussian_ijk = (gUs[i][j][k]'^uv' * Rs[i][j][k]'_uv')()
+			for a=1,4 do
+				for b=a,4 do	-- do I have to also subst symmetric or no?
+					--printbr(Gs[ijkab]:eq(Rs[ijkab]  - frac(1,2) * gs[ijkab] * gUs[ijk]'^uv' * Rs[ijk]'_uv'))
+					grad_Phi_def = grad_Phi_def:subst(
+						Gs[i][j][k][a][b]:eq(Rs[i][j][k][a][b]  - frac(1,2) * gs[i][j][k][a][b] * Gaussian_ijk)
+					)
+				end
+			end
+		end
+	end
 end
 printbr(grad_Phi_def)
-grad_Phi_def = grad_Phi_def()
+-- TODO i don't know that d/dg_ab g^ab works anymore ...
+-- now this is going too slow ...
+grad_Phi_def = grad_Phi_def:simplifyAddMulDiv()
 printbr(grad_Phi_def)
+do return end
 
---[[ TODO ensure d/dg_pq g_ab = delta^p_a delta^q_b
--- and d/dg_pq g^ab = -g^pa g^qb
-printbr(gs[n+1][n+1][n+1]'_ab':diff(gs[n+1][n+1][n+1]'_pq'):eq(
-		gs[n+1][n+1][n+1]'_ab':diff(gs[n+1][n+1][n+1]'_pq')()
-	))
-printbr(gs[n+1][n+1][n+1]'^ab':diff(gs[n+1][n+1][n+1]'_pq'):eq(
-		gs[n+1][n+1][n+1]'^ab':diff(gs[n+1][n+1][n+1]'_pq')()
-	))
---]]
+-- ok we might need to be expanding all indexes ...
+-- because here I have to sum the particular change-in-grid-index with its associated product's tensor-index
+-- i.e. f^a g_,a means Sum_j f^j[i] g[i+dx^j] - f^j[i] g[i-dx^j] = f^1[i] g[i1+1,i2] - f^1[i] g[i1-1,i2] + f^2[i] g[i1,i2+1] - f^2[i] g[i1,i2-1]
+local function finDiff2(T, ij)
+end
 
---[[
+-- [[
 local delta = Tensor:deltaSymbol()
-for i,G in Gs:iter() do
+for ijk,R_ijk in Rs:iter() do
+	local g_ijk = gs[ijk]
+	grad_Phi_def = grad_Phi_def:subst(
+		R_ijk'_ab':eq(
+			g_ijk'^uv' * (
+				frac(1,2) * (
+					  finDiff2(gs, '_ub')'_av'
+					+ finDiff2(gs, '_av')'_ub'
+					- finDiff2(gs, '_uv')'_ab'
+					- finDiff2(gs, '_ab')'_uv'
+				)
+				--[=[
+				+ g_ijk'^ef' * (
+-- store Gamma? 
+--					makeGamma('_e', '_u', '_b') * makeGamma('_f', '_a', '_v')
+--					- makeGamma('_e', '_u', '_v') * makeGamma('_f', '_a', '_b')
+-- or calculate it on the fly?
+				)
+				--]=]
+			)
+		)
+	)
+
 	local function makeGamma(a,b,c)
 		return frac(1,2) * (
-			finiteDifference(g(a)(b), i)(c)
-			+ finiteDifference(g(a)(c), i)(b)
-			- finiteDifference(g(b)(c), i)(a)
+			finDiff(g(a)(b), i)(c)
+			+ finDiff(g(a)(c), i)(b)
+			- finDiff(g(b)(c), i)(a)
 		)
 	end
-	--grad_Phi_def = grad_Phi_def:replace(Gs[i]'_ab', Rs[i]'_ab' - frac(1,2) * gs[i]'_ab' * Rs[i])
 	grad_Phi_def = grad_Phi_def:replace(
 		Gs[i]'_ab',
-		(gs[i]'^uv' * delta'^c_a' * delta'^d_b' - frac(1,2) * gs[i]'_ab' * gs[i]'^uv' * gs[i]'^cd') * Rs[i]'_ucvd'
+		(
+			gs[i]'^uv' * delta'^c_a' * delta'^d_b' 
+			- frac(1,2) * gs[i]'_ab' * gs[i]'^uv' * gs[i]'^cd'
+		) * Rs[i]'_ucvd'
 	)
 	--[=[
 	:replace(
